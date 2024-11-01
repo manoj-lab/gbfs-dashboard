a1f05f00ee3f1a4517d07f46201d0f1657042c0f
Manoj Pannir <manoj89.p@live.com>
2024-11-01 07:36:15 +0530

    update 4

--- terraform/backend/backend_setup.tf
+++ terraform/backend/backend_setup.tf
@@ -5,21 +5,8 @@ provider "aws" {
 # S3 Bucket for Terraform State
 resource "aws_s3_bucket" "terraform_state_bucket" {
   bucket = "my-gbfs-terraform-state-bucket"
-
-  #Define lifecycle rules if you want to archive/delete old state versions
-  lifecycle_rule {
-    enabled = true
-
-    noncurrent_version_transition {
-      days          = 30
-      storage_class = "GLACIER"
-    }
-
-    noncurrent_version_expiration {
-      days = 365
-    }
-  }
 }
+
 # Enable versioning for state management
 resource "aws_s3_bucket_versioning" "terraform_state_bucket" {
   bucket = aws_s3_bucket.terraform_state_bucket.id
@@ -27,3 +14,20 @@ resource "aws_s3_bucket_versioning" "terraform_state_bucket" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "versioning-bucket-config" {
+  depends_on = [aws_s3_bucket_versioning.terraform_state_bucket]
+
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+
+  noncurrent_version_expiration {
+      noncurrent_days = 90
+    }
+
+  noncurrent_version_transition {
+      noncurrent_days = 30
+      storage_class   = "GLACIER"
+    }
+    status = "Enabled"
+}
+
